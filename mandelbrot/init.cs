2c36c26d3808f0de8a00671de1fe192f6d0b6703
koko <github@koko-campus.com>
2022-09-14 19:33:13 +0900

    GIFアニメーション化。

--- mandelbrot/init.cs
+++ mandelbrot/init.cs
@@ -4,18 +4,21 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.Modeling.Diagrams;
 using System.Numerics;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 internal static partial class Program
 {
-    static int width = 5000;
-    static int height = 5000;
-    static int resolution = 1200;
-    static double Re = 2.5F;
-    static double Im = 1.2F;
+    static int width = 500;
+    static int height = 500;
+    static double resolution = 200;
+    static double Re = 0.5;
+    static double Im = 2.0;
     static int hue = 0;
     static int limit = 20;
     static int upto = 10;
     static string file = "";
+    static int count = 30;
 
     internal static int Main(string[] args)
     {
@@ -23,31 +26,49 @@ internal static partial class Program
 
         if (IsNum(Obtain("-w"))) width = int.Parse(Obtain("-w"));
         if (IsNum(Obtain("-h"))) height = int.Parse(Obtain("-h"));
-        if (IsNum(Obtain("-rs"))) resolution = int.Parse(Obtain("-rs"));
+        if (IsNum(Obtain("-rs"))) resolution = double.Parse(Obtain("-rs"));
         if (IsNum(Obtain("-re"))) Re = double.Parse(Obtain("-re"));
         if (IsNum(Obtain("-im"))) Im = double.Parse(Obtain("-im"));
         if (IsNum(Obtain("-hue"))) hue = int.Parse(Obtain("-hue"));
         if (IsNum(Obtain("-limit"))) limit = int.Parse(Obtain("-limit"));
         if (IsNum(Obtain("-upto"))) upto = int.Parse(Obtain("-upto"));
-        file = Obtain("-f") != "" ? Obtain("-f") : $@"W{width}__H{height}__R{resolution}__RE{Re}__Im{Im}";
+        if (IsNum(Obtain("-count"))) count = int.Parse(Obtain("-count"));
+        file = Obtain("-f") != "" ? Obtain("-f") : $@"1.gif";
 
         if (File.Exists(file)) File.Delete(file);
-        Bitmap canvas = new(width, height);
-        
-        for (double x = 0; x < width; x++)
+
+        Bitmap[] gifs = new Bitmap[count];
+        List<Task> tasks = new List<Task>();
+
+        foreach (var counter in Enumerable.Range(0, count))
         {
-            for (double y = 0; y < height; y++)
-            {
-                int result = Check(x / resolution - Re, y / resolution - (3 - Im));
-                Color color = Color.Black;
-                if (result != 0)
+            var task = Task.Run(() => {
+                Console.WriteLine($"{counter} started / out of ({count})");
+                resolution *= 1.05;
+                Bitmap canvas = new(width, height);
+                for (double x = 0; x < width; x++)
                 {
-                    color = new HslColor(hue, 100, result).ToRgbColor();
+                    for (double y = 0; y < height; y++)
+                    {
+                        int result = Check(x / resolution - Re, y / resolution - 3 + Im);
+                        Color color = Color.Black;
+                        if (result != 0)
+                        {
+                            color = new HslColor(hue, 100, result).ToRgbColor();
+                        }
+                        canvas.SetPixel((int)x, (int)y, color);
+                    }
                 }
-                canvas.SetPixel((int)x, (int)y, color);
-            }
+                gifs[counter] = canvas;
+                Console.WriteLine($"{counter} ended / out of ({count})");
+            });
+            tasks.Add(task);
+            Task.WhenAll(tasks).Wait();
         }
-        canvas.Save(file);
+        Task.WhenAll(tasks).Wait();
+
+        SaveAnimatedGif(file, gifs, 10, 0);
+
         return 0;
     }
 
