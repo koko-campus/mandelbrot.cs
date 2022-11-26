dc8b54f920f27c09ced028ddb0aa3d6c305847e1
osawa-koki <github@osawa.dev>
2022-11-27 00:48:44 +0900

    コードスタイル調整。

--- mandelbrot/parser.cs
+++ mandelbrot/parser.cs
@@ -7,18 +7,18 @@ using System.Threading.Tasks;
 
 internal static partial class Program
 {
-    static readonly Dictionary<string, string> answer = new();
-    private static void Parse(string[] args)
+  static readonly Dictionary<string, string> answer = new();
+  private static void Parse(string[] args)
+  {
+    for (int i = 0; i < args.Length; i++)
     {
-        for (int i = 0; i < args.Length; i++)
-        {
-            if (args.Length - 1 < i + 1) continue;
-            answer[args[i]] = args[i + 1];
-            i++;
-        }
+      if (args.Length - 1 < i + 1) continue;
+      answer[args[i]] = args[i + 1];
+      i++;
     }
-    private static string Obtain(string key)
-    {
-        return answer.ContainsKey(key) ? answer[key] : "";
-    }
+  }
+  private static string Obtain(string key)
+  {
+    return answer.ContainsKey(key) ? answer[key] : "";
+  }
 }
