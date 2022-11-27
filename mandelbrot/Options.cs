d70b2ee4d2847d644256cfde861be1061e913982
osawa-koki <github@osawa.dev>
2022-11-27 14:18:37 +0900

    オプション設定。

--- mandelbrot/Options.cs
+++ mandelbrot/Options.cs
@@ -8,30 +8,30 @@ class Options
   [Option('h', "height", Required = false, HelpText = "画像の縦幅を指定します。")]
   internal int Height { get; set; } = 500;
   
-  [Option('r', "resolution", Required = false, HelpText = "解像度を指定します。")]
+  [Option('s', "resolution", Required = false, HelpText = "解像度を指定します。")]
   internal double Resolution { get; set; } = 200;
   
-  [Option("re", Required = false, HelpText = "実軸の開始点を指定します。")]
+  [Option('r', "re", Required = false, HelpText = "実軸の開始点を指定します。")]
   internal double Re { get; set; } = 0.5;
 
-  [Option("im", Required = false, HelpText = "虚軸の開始点を指定します。")]
+  [Option('i', "im", Required = false, HelpText = "虚軸の開始点を指定します。")]
   internal double Im { get; set; } = -1.0;
 
-  [Option("hue", Required = false, HelpText = "集合の着色を色相を指定します。")]
+  [Option('h', "hue", Required = false, HelpText = "集合の着色を色相を指定します。")]
   internal int Hue { get; set; } = 0;
 
-  [Option("limit", Required = false, HelpText = "収束判定の閾値を指定します。")]
+  [Option('l', "limit", Required = false, HelpText = "収束判定の閾値を指定します。")]
   internal int Limit { get; set; } = 20;
 
-  [Option("max", Required = false, HelpText = "最大反復回数を指定します。")]
+  [Option('x', "max", Required = false, HelpText = "最大反復回数を指定します。")]
   internal int Upto { get; set; } = 10;
   
-  [Option("file", Required = false, HelpText = "出力先ファイル名を指定します。")]
+  [Option('f', "file", Required = false, HelpText = "出力先ファイル名を指定します。")]
   internal string File { get; set; } = "mandelbrot.gif";
 
-  [Option("zoom", Required = false, HelpText = "ズームレベルを設定します。")]
-  internal double zoom { get; set; } = 1.05;
-  
-  [Option("count", Required = false, HelpText = "カウントを指定します。")]
+  [Option('z', "zoom", Required = false, HelpText = "ズームレベルを設定します。")]
+  internal double Zoom { get; set; } = 1.05;
+
+  [Option('c', "count", Required = false, HelpText = "カウントを指定します。")]
   internal int Count { get; set; } = 30;
 }
